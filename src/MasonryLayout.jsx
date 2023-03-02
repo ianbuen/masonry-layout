59cbb84e4ab4259009557ff1dd6521cb54eae993
ianbuen <ianbuen@gmail.com>
2023-03-02 16:17:53 +0800

    add resize listener

--- src/MasonryLayout.jsx
+++ src/MasonryLayout.jsx
@@ -1,71 +1,108 @@
-import "./MasonryLayout.sass";
+import { useEffect, useState } from "react";
+import "./MasonryLayout.sass"
 
 export const MasonryLayout = ({ data }) => {
-  return <div className="MasonryLayout">
-    {data.map(({ title, image, text }) =>
-        <div className="Card" data-count={data.length} >
-            <div className="Image" style={{background: image}}></div>
-            <h1 className="Title">{title}</h1>
-            <p className="Text">{text}</p>
-        </div>
-    )}
-  </div>;
+
+    const [intColumns, setColumns] = useState(1)
+    const [arrColumns, setArrColumns] = useState(null);
+
+    useEffect(() => {
+        const updateMedia = () => {
+            if (innerWidth < 720)
+                setColumns(1);
+            else if (innerWidth < 1080)
+                setColumns(2);
+            else
+                setColumns(3);
+
+            setArrColumns(Array(intColumns).fill([]));
+            console.log(arrColumns);
+
+            data.forEach((item, index) => {
+                console.log(Card(item));
+            });
+        }
+
+      updateMedia();
+
+      window.addEventListener('resize', updateMedia);
+    
+      return () => window.removeEventListener('resize', updateMedia);
+    }, []);
+
+    return <div className="MasonryLayout">
+        <p style={{color: "black", position: "absolute", top: 0, left: 0}}>{`${intColumns}`}</p>
+        {data.map(item => Card(item))}
+    </div>;
 };
 
-const randomLengthText = () => {
-    let text = "The quick brown fox jumps over the lazy dog. ";
+const Column = () => <div className="Column"></div>;
 
-    for (var i = 0; i < Math.floor((Math.random() * 4)); i++) {
-         text += text;
+const Card = ({title, image, text}) =>
+    <div className="Card" >
+        <div className="Image" style={{background: image}}></div>
+        <h1 className="Title">Color Hex {title}</h1>
+        <p className="Text">{text}</p>
+    </div>
+
+const randomLengthText = () => {
+    let text = "";
+
+    for (var i = 0; i < Math.floor((Math.random() * 20)) + 1; i++) {
+         text += "The quick brown fox jumps over the lazy dog. ";
     }
          
     return text;
 };
 
+const randomColor = () => {
+    return `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0').toUpperCase()}`
+};
+
 export const data = [
   {
-    title: "The Big Black Jet",
-    image: "#556699",
+    title: randomColor(),
+    image: randomColor(),
     text: randomLengthText(),
   },
   {
-    title: "Yummy Borgir",
-    image: "#996699",
+    title: randomColor(),
+    image: randomColor(),
     text: randomLengthText(),
   },
   {
-    title: "That One Artist",
-    image: "#ffffcc",
+    title: randomColor(),
+    image: randomColor(),
     text: randomLengthText(),
   },
   {
-    title: "That One Artist",
-    image: "#2222dd",
+    title: randomColor(),
+    image: randomColor(),
     text: randomLengthText(),
   },
   {
-    title: "That One Artist",
-    image: "#55ff33",
+    title: randomColor(),
+    image: randomColor(),
     text: randomLengthText(),
   },
   {
-    title: "That One Artist",
-    image: "#233333",
+    title: randomColor(),
+    image: randomColor(),
     text: randomLengthText(),
   },
   {
-    title: "That One Artist",
-    image: "#8723fc",
+    title: randomColor(),
+    image: randomColor(),
     text: randomLengthText(),
   },
   {
-    title: "That One Artist",
-    image: "#439878",
+    title: randomColor(),
+    image: randomColor(),
     text: randomLengthText(),
   },
   {
-    title: "That One Artist",
-    image: "#55ffcc",
+    title: randomColor(),
+    image: randomColor(),
     text: randomLengthText(),
   },
 ];
